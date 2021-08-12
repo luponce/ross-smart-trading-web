2fc94f941b1f113c8ded0a65bc0d4c1c6598e3e6
Monesterolo Pedro <pmonesterolo@sooft.com.ar>
2021-08-12 11:03:24 -0300

    Correcciones URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // api_URL: 'http://127.0.0.1:8000/api/',
-  api_URL: 'http://nubuspyme.com.ar:85/',
+  api_URL: 'http://nubuspyme.com.ar:85/api/',
   telefono: '832782387923'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   // api_URL: 'http://127.0.0.1:8000/api/',
-  api_URL: 'http://nubuspyme.com.ar:85/',
+  api_URL: 'http://nubuspyme.com.ar:85/api/',
   telefono: '832782387923'
 };
 
